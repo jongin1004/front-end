53cfa82c8812e2475282e4c4e24aa8383e890283
bill1224 <bill1224@naver.com>
2021-09-27 23:38:56 +0900

    portfolio -> header

---
+++ portfolio/ver2/main.js
@@ -0,0 +1,31 @@
+// header
+const menuBtn = document.querySelector(".menu_bar");
+const menu = document.querySelector(".header_menu");
+
+menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+
+// banner-typing
+let typingText = document.querySelector(".typing-text").textContent;
+typingText = typingText.split("");
+let typingTarget = document.querySelector(".typing");
+let typingIdx = 0;
+let typingBool = false;
+let typingSet;
+
+if (typingBool == false) {
+    typingBool = true;
+
+    typingSet = setInterval(typing, 150);
+}
+
+function typing() {
+    if(typingIdx < typingText.length) {        
+        typingTarget.append(typingText[typingIdx]);
+        typingIdx ++;
+    } else {
+        clearInterval(typingSet);
+    }
+}
