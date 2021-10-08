f8edd6e98e43d214b62f8aac442118a22deb4642
jongin1004 <bill1224@naver.com>
2021-10-08 20:00:19 +0900

    modify JS -> 아코디언 선택할 때, 다른 아코디언은 닫히도록

--- responsive_web/acodian/main.js
+++ responsive_web/acodian/main.js
@@ -2,8 +2,21 @@ const title = document.querySelectorAll(".title");
 const con = document.querySelectorAll(".con");
 
 for(let i = 0; i < title.length; i ++) {
-    title[i].addEventListener("click", () => {
-        con[i].classList.toggle("active");
-    });
-}
+    title[i].addEventListener("click", () => {   
+        let bool = true;        
+        if (con[i].className == "con active") {
+            con[i].classList.remove("active");            
+            bool = false;                     
+        }        
 
+        if (bool) {
+            for(let j = 0; j < con.length; j ++) {
+                if (i == j) {
+                    con[j].classList.add("active");
+                } else {
+                    con[j].classList.remove("active");
+                }
+            }   
+        }        
+    });    
+}
