c9f5377c5a26c4809aabe61a235d8ca17f37d89d
jongin1004 <bill1224@naver.com>
2021-10-08 20:00:38 +0900

    add acodian

--- portfolio/ver2/main.js
+++ portfolio/ver2/main.js
@@ -120,3 +120,29 @@ window.onclick = function(event) {
 // delay(document.querySelector('.image_container'), function() {
 //     document.querySelector('.modalCaptin').classList.add("active");
 // });
+
+
+// 아코디언
+const title = document.querySelectorAll(".aco_title");
+const con = document.querySelectorAll(".con");
+
+
+for(let i = 0; i < title.length; i ++) {
+    title[i].addEventListener("click", () => {   
+        let bool = true;        
+        if (con[i].className == "con active") {
+            con[i].classList.remove("active");            
+            bool = false;                     
+        }        
+
+        if (bool) {
+            for(let j = 0; j < con.length; j ++) {
+                if (i == j) {
+                    con[j].classList.add("active");
+                } else {
+                    con[j].classList.remove("active");
+                }
+            }   
+        }        
+    });    
+}
