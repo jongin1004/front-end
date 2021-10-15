f3f22d9207a0d21836e008d3a8aa9206b1d970fe
jongin1004 <bill1224@naver.com>
2021-10-15 23:34:04 +0900

    integrate CSS file

--- API/DataAPI/travelAPI_project/resources/js/components/Main.vue
+++ API/DataAPI/travelAPI_project/resources/js/components/Main.vue
@@ -1,12 +1,12 @@
 <template>    
-
     <div class="wrapper" v-if="categoryState === 'main'">
         <div v-for="(menu, i) in menus" :key="i" @click="changeCategory(menu)">
             {{ menu }}
         </div>
-    </div>        
+    </div>    
+    <!-- 코로나 소식 -->
     <Covid v-else-if="categoryState === '코로나소식'"/>
-
+    <!-- 해외입국자 조치현황 -->
     <StatusOfEntry v-else-if="categoryState === '해외입국자 조치현황'" />
 </template>
 
@@ -43,42 +43,5 @@ export default {
 </script>
 
 <style>
-.wrapper {
-    display: flex;
-    flex-grow: 1;
-    flex-wrap: wrap;
-    justify-content: center;
-    /* align-items: center; */
-    /* height: 100%; */
-}
-
-.wrapper > div {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 270px;
-    height: 150px;
-    border: 1px solid #000;
-    border-radius: 4px;
-    margin: 4px;
-}
-
-.wrapper > div:hover {
-    color: white;
-    background-color: #0093c4;
-}
-
-@media screen and (max-width: 768px) {
-    .wrapper > div {
-        width: 200px;
-        height: 75px;
-    }
-}
-
-@media screen and (max-width: 520px) {
-    .wrapper > div {
-        width: 100px;
-        height: 50px;
-    }
-}
+@import '../../css/app.css';
 </style>
--- API/DataAPI/travelAPI_project/resources/js/components/StatusOfEntry.vue
+++ API/DataAPI/travelAPI_project/resources/js/components/StatusOfEntry.vue
@@ -34,6 +34,7 @@
 <script>
 import pagination from './pagination';
 
+
 export default {
     components: {
         pagination,
@@ -48,14 +49,13 @@ export default {
             detailContent: '',
             countries: '',            
             pageInfo: '',
-            countryAll: '',                  
+            countryAll: '',  //search할 때, 필요한 country의 모든 데이터를 담기 위함   
         }
     },
 
-    created() {                
+    created() {                        
         axios.get('api/allCountry').then(res => {            
-            this.countryAll = res.data.countries;
-            console.log(res.data.countries);
+            this.countryAll = res.data.countries;            
         });
 
         this.getCountry();        
@@ -107,106 +107,3 @@ export default {
     }
 }
 </script>
-
-<style>
-.content {    
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    height: 100%;
-}
-
-.content .search {
-    flex-basis: 20%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-}
-
-.content .searchText{
-    width: 100%;
-    border-radius: 4px;
-    font-size: 12px;
-    padding-left: 20px;
-}
-
-.content i {
-    position: absolute;
-    left: 4px;
-    font-size: 8px;
-}
-
-.content .list {
-    flex-basis: 60%;
-    border: 1px solid #000;
-    border-radius: 4px; 
-    padding: 4px;
-    background-color: #b6ffff;
-}
-
-.content .list > div {
-    border-bottom: 1px solid #000;    
-    margin-bottom: 4px;
-    padding: 8px;
-    font-size: 8px;
-}
-
-
-.content .list > div:hover {
-    color: white;
-    background-color: #0093c4;
-}
-
-/* modal */
-.modal {    
-    position: fixed;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    top: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
-    z-index: -1;    
-    opacity: 0;
-    transition: all .5s;
-}
-
-.modal.active {
-    opacity: 1;
-    z-index: 30;
-}
-
-.modal_content {    
-    display: flex;
-    padding: 5vw;
-    width: 85%; 
-    height: 90%;
-    border-radius: 1vw;
-    overflow-y: auto;    
-    background-color: #4b636e;
-    color: #a7c0cd;
-    border: 3px solid #a7c0cd;
-}
-
-.modal i {    
-    color: rgba(0, 0, 0, 0.6);    
-    transition: all .5s;
-    margin: 0 8px;
-}
-
-.modal i:hover {
-    color: rgb(0, 0, 0);
-    transform: scale(1.5);
-}
-
-/* pagination */
-.pagination {
-    width: 100%;
-    /* text-align: center; */
-    /* margin: 8px; */
-}
-
-</style>
